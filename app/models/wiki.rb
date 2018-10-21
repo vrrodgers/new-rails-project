75c8e642b22a7469e4931b2c481ea0cb62cc32bc
vrrodgers <trulymissvicky1982@gmail.com>
2018-10-21 15:48:30 -0500

    added admin roles with enum

--- app/models/wiki.rb
+++ app/models/wiki.rb
@@ -1,4 +1,14 @@
 class Wiki < ApplicationRecord
   belongs_to :user
-  
+
+  before_action :require_sign_in, except: [:index, :show]
+  before_action :authorize_user, except: [:index, :show]
+
+
+  def authorize_user
+     unless current_user.admin?
+       flash[:alert] = "You must be an admin to do that."
+       redirect_to wikis_path
+     end
+   end
 end
